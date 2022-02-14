dda99f48f0b5787d7a3b996e91f1ab1318a3f951
LinYenCheng <breaking0215@hotmail.com>
2022-02-14 09:30:36 +0800

    refactor(PopupMarker.jsx): 把 PopupMarker 獨立

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,74 +1,12 @@
 /* eslint-disable react/no-danger */
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 import PropTypes from 'prop-types';
 
-function getStars(num) {
-  switch (num) {
-    case 5:
-      return '★★★★★';
-    case 4:
-      return '★★★★';
-    case 3:
-      return '★★★';
-    case 2:
-      return '★★';
-    default:
-      return '★';
-  }
-}
+import PopupMarker from './PopupMarker';
 
-const PopupMarker = ({ position, item, isActive, map }) => {
-  const { url, name, address, wifi, seat, quiet, tasty, cheap, music } = item;
-  const [refReady, setRefReady] = useState(false);
-  let popupRef = useRef();
-
-  useEffect(() => {
-    if (refReady && isActive) {
-      popupRef.openOn(map);
-      map.panTo({ lat: position[0], lng: position[1] });
-    }
-  }, [isActive, refReady, map, position]);
-  if (!isActive) return null;
-
-  return (
-    <Marker position={{ lat: position[0], lng: position[1] }}>
-      <Popup
-        ref={(r) => {
-          popupRef = r;
-          setRefReady(true);
-        }}
-      >
-        <div>
-          <span style={{ fontWeight: 800, fontSize: '16px' }}>{name}</span>
-          <br />
-          <span>
-            {address}
-            <br />
-            {wifi > 0 ? `WIFI穩定: ${getStars(wifi)} ` : ' '}
-            {wifi > 0 ? <br /> : ''}
-            {seat > 0 ? `通常有位:  ${getStars(seat)} ` : ' '}
-            {seat > 0 ? <br /> : ''}
-            {quiet > 0 ? `安靜程度:  ${getStars(quiet)} ` : ' '}
-            {quiet > 0 ? <br /> : ''}
-            {tasty > 0 ? `咖啡好喝:  ${getStars(tasty)} ` : ' '}
-            {tasty > 0 ? <br /> : ''}
-            {cheap > 0 ? `價格便宜:  ${getStars(cheap)} ` : ' '}
-            {cheap > 0 ? <br /> : ''}
-            {music > 0 ? `裝潢音樂:  ${getStars(music)} ` : ' '}
-            {music > 0 ? <br /> : ''}
-          </span>
-          {url ? <a href={url}>粉絲專頁</a> : ''}
-        </div>
-      </Popup>
-    </Marker>
-  );
-};
-
-function MyMapEvent({ isMenuOpen, toggleMenu, setMapBounds }) {
-  // eslint-disable-next-line no-unused-vars
-
+function CoffeeMapEvent({ isMenuOpen, toggleMenu, setMapBounds }) {
   const map = useMapEvents({
     click: () => {
       if (isMenuOpen) toggleMenu();
@@ -123,7 +61,7 @@ const CoffeeMap = React.memo(({ position, isMenuOpen, toggleMenu, item, items, s
       animate
       whenCreated={setMap}
     >
-      <MyMapEvent isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} setMapBounds={setMapBounds} />
+      <CoffeeMapEvent isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} setMapBounds={setMapBounds} />
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
