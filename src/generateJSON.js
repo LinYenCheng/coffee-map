f21044b22e118030cebdf23654253baa1eac09b0
LinYenCheng <breaking0215@hotmail.com>
2023-11-19 09:54:13 +0800

    chore: update data

--- src/generateJSON.js
+++ src/generateJSON.js
@@ -5,88 +5,88 @@ const cities = [
   {
     name: 'taipei',
     displayName: '台北',
-    checked: false
+    checked: false,
   },
   {
     name: 'keelung',
     displayName: '基隆',
-    checked: false
+    checked: false,
   },
   {
     name: 'taoyuan',
     displayName: '桃園',
-    checked: false
+    checked: false,
   },
   {
     name: 'hsinchu',
     displayName: '新竹',
-    checked: true
+    checked: true,
   },
   {
     name: 'miaoli',
     displayName: '苗栗',
-    checked: false
+    checked: false,
   },
   {
     name: 'taichung',
     displayName: '台中',
-    checked: false
+    checked: false,
   },
   {
     name: 'changhua',
     displayName: '彰化',
-    checked: false
+    checked: false,
   },
   {
     name: 'nantou',
     displayName: '南投',
-    checked: false
+    checked: false,
   },
   {
     name: 'yunlin',
     displayName: '雲林',
-    checked: false
+    checked: false,
   },
   {
     name: 'chiayi',
     displayName: '嘉義',
-    checked: false
+    checked: false,
   },
   {
     name: 'tainan',
     displayName: '台南',
-    checked: false
+    checked: false,
   },
   {
     name: 'kaohsiung',
     displayName: '高雄',
-    checked: false
+    checked: false,
   },
   {
     name: 'pingtung',
     displayName: '屏東',
-    checked: false
+    checked: false,
   },
   {
     name: 'yilan',
     displayName: '宜蘭',
-    checked: false
+    checked: false,
   },
   {
     name: 'hualien',
     displayName: '花蓮',
-    checked: false
+    checked: false,
   },
   {
     name: 'taitung',
     displayName: '台東',
-    checked: false
+    checked: false,
   },
   {
     name: 'penghu',
     displayName: '澎湖',
-    checked: false
-  }
+    checked: false,
+  },
 ];
 
 function writeResToJOSON(res, fileName) {
@@ -97,11 +97,15 @@ function writeResToJOSON(res, fileName) {
 const instance = axios.create({
   baseURL: 'https://cafenomad.tw/api/v1.2',
   timeout: 10000,
-  headers: { 'X-Custom-Header': 'foobar' }
+  headers: { 'X-Custom-Header': 'foobar' },
 });
 
-cities.forEach(city => {
-  instance.get(`/cafes/${city.name}`).then(res => {
+cities.forEach((city) => {
+  instance.get(`/cafes/${city.name}`).then((res) => {
     writeResToJOSON(res, city.name);
   });
+});
+
+instance.get(`/cafes`).then((res) => {
+  writeResToJOSON(res, 'taiwan');
 });
