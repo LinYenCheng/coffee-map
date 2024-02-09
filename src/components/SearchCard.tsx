12ca2018fa317b874eeb78e156d58cfc8617e378
LinYenCheng <breaking0215@hotmail.com>
2024-02-09 14:12:41 +0800

    style: modify style

--- src/components/SearchCard.tsx
+++ src/components/SearchCard.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { Tooltip } from 'primereact/tooltip';
 
 import srcMRT from './assets/icon_mrt.png';
 import srcChair from './assets/icon_chair.png';
@@ -51,7 +52,7 @@ function SearchCard({ item, onSelect }: SearchCardProps): JSX.Element {
   return (
     <div
       role="presentation"
-      className="card pt-3 ps-4"
+      className="card pt-3 ps-3"
       onClick={() => {
         onSelect(item);
       }}
@@ -62,20 +63,26 @@ function SearchCard({ item, onSelect }: SearchCardProps): JSX.Element {
         onSelect(item);
       }}
     >
-      <div className="card__title">
-        <a
-          className="h4"
-          href={item.url}
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-        >
-          {name}
-        </a>
-        <span className="ms-2 score">{score}</span>
-        <i className="pi pi-star-fill score ms-1"></i>
+      <div className="card__title d-flex justify-content-start">
+        <div className="title">
+          <Tooltip target=".a-title" position="bottom" />
+          <a
+            data-pr-tooltip={name}
+            className="h4 a-title"
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            {name}
+          </a>
+        </div>
+        <div className="">
+          <span className="ms-2 score">{score}</span>
+          <i className="pi pi-star-fill score ms-1"></i>
+        </div>
       </div>
       <ul>
         <li>
